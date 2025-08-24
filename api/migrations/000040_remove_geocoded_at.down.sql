668416f263bb4b88d31dd7a2ad257e124707b3a9
Tyrel Chambers <tychambers3@gmail.com>
2025-08-24 12:10:56 -0400

    fix

--- api/migrations/000040_remove_geocoded_at.down.sql
+++
@@ -1,2 +0,0 @@
-alter table non_compliant_employers add column geocoded_at timestamp;
-alter table non_compliant_employers add column reason_id integer;
--- api/migrations/000040_remove_geocoded_at.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE non_compliant_employers DROP COLUMN geocoded_at;
-ALTER TABLE non_compliant_employers DROP COLUMN reason_id
